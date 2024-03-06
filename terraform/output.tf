b5389b239461ef5c7d8acdd9e40d1ae3e3d9b134
Florian Rumiel <rumiel.florian@gmail.com>
2024-03-06 15:23:33 -0500

    Update deploy-s3.yml and output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,7 @@ output "s3_bucket_website_url_domain" {
 output "s3_bucket_website_url" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
+
+output "bucket" {
+  value = aws_s3_bucket.deploy.bucket
+}
