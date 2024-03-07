3c2b41a77878b5146f1e703307c2cfc1de540390
Florian Rumiel <rumiel.florian@gmail.com>
2024-03-07 09:00:11 -0500

    Refactor PromptInspector widget to remove unused code

--- lib/presentation/rag_settings/prompt_inspector.dart
+++ lib/presentation/rag_settings/prompt_inspector.dart
@@ -9,30 +9,9 @@ class PromptInspector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var regularSpans = prompt?.split(RegExp(r'<(\w+)>(.*?)<\/\1>', dotAll: true)) ?? [];
-    var quoteReg = RegExp(r'<quotes>(.*?)<\quotes>', dotAll: true);
-    var historyReg = RegExp(r'<history>(.*?)<\history>', dotAll: true);
-
-    // String? quotes = quoteReg.allMatches(prompt ?? '').map((e) => e.group(1)).first;
-    // String? history = historyReg.allMatches(prompt ?? '').map((e) => e.group(1)).first;
-
     return SingleChildScrollView(
-      child: RichText(
-        text: TextSpan(
-          children: regularSpans.map((span) {
-            if (span.startsWith('<') && span.endsWith('>')) {
-              return TextSpan(
-                text: span,
-                style: const TextStyle(color: Colors.red), // Change color for XML tags
-              );
-            } else {
-              return TextSpan(
-                text: span,
-                style: const TextStyle(color: Colors.black), // Change color for other text
-              );
-            }
-          }).toList(),
-        ),
+      child: Text(
+        prompt ?? '',
       ),
     );
   }
